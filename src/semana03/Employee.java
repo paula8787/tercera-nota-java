a8a82353c630c0dbcf20402a6c44a499ef73a0cc
paula <echavezp02@gmail.com>
2025-09-03 19:49:23 -0500

    primer commit

---
+++ src/semana03/Employee.java
@@ -0,0 +1,11 @@
+package semana03;
+
+public class Employee {
+    public String name;
+    private double salary;
+    protected String role;
+    
+    public Employee(String name, String role, double salary) {
+        
+    }
+}

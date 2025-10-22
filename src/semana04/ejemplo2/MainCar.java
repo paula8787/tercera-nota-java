94e398bfdc0deba7df2a38d13b41f17ef88b0d5f
Pau <echavezp02@gmail.com>
2025-10-22 12:07:06 -0500

    Ejercicios

--- src/semana04/ejemplo2/MainCar.java
+++
@@ -1,8 +0,0 @@
-package semana04.ejemplo2;
-
-public class MainCar {
-    public static void main(String[] args) {
-        Car myCar = new Car("Toyota", 2020);
-        myCar.displayInfo();
-    }   
-}
